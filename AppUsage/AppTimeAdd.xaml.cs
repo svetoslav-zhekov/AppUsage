264fbc168521b57d6faad9957fd632fcbda22c5c
Svetoslav Zhekov <svetoslav.zhekov@abv.bg>
2021-11-29 18:58:58 +0200

    Cleaned up code.

--- AppUsage/AppTimeAdd.xaml.cs
+++ AppUsage/AppTimeAdd.xaml.cs
@@ -1,20 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.Diagnostics;
-using Newtonsoft.Json;
-using System.IO;
-using System.Security.AccessControl;
 
 namespace AppUsage
 {
